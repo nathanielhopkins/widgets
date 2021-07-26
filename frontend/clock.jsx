c28252d921e3fc546e1055c2273d0ac96e52d6e8
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-26 15:04:52 -0700

    write constructor for Clock, set initial state of time to new Date()

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { ProgressPlugin } from 'webpack';
 
 export default class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+  }
+  
   render() {
     return(
       <div>
