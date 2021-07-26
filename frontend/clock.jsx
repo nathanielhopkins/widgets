f67b2c34d939a5283990287227197781632435b6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-26 15:08:06 -0700

    write method tick() for Clock that calls this.setState to make time a new Date(); bind this.tick to this in constructor

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -7,6 +7,12 @@ export default class Clock extends React.Component {
     this.state = {
       time: new Date()
     };
+
+    this.tick = this.tick.bind(this);
+  }
+
+  tick() {
+    this.setState({time: new Date()});
   }
   
   render() {
