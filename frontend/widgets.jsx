7e49b27c0d2534f13fce475164a3353a1ca4b1c6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-27 12:54:52 -0700

    import Tabs in widgets.jsx; create array of tab objects as a variable and load it as prop tabs in a Tab in Root

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+let tabs = [
+  { 'title': 'tab1', 'content': 'this is the first tab' },
+  { 'title': 'tab2', 'content': 'this is the second tab' },
+  { 'title': 'tab3', 'content': 'this is the third tab' }
+];
 
 function Root() {
   return(
     <div>
       <Clock />
+      <Tabs tabs={tabs}/>
     </div>
   );
 }
