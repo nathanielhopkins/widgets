c62582071bd5edea6dc28e674b57f2dfe6254164
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-29 12:55:24 -0700

    flesh out render of Tabs; write React Component Headers that helps handle the Tab headers in render and keep track of which tab is selected; render content of selected tab only

--- frontend/tabs.jsx
+++ frontend/tabs.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+class Headers extends React.Component {
+  render() {
+    let selectedTab = this.props.selectedTab;
+    let tabs = this.props.tabs;
+    let headers = tabs.map((tab,index) => {
+      let title = tab.title;
+      let isSelected = index === selectedTab ? 'selected' : '';
+
+      return (
+        <li 
+          key = {index}
+          className = {isSelected}>
+          {title}
+        </li>
+      )
+    });
+
+    return(
+      <ul className="tab-headers">
+        {headers}
+      </ul>
+    );
+  }
+}
+
+
 export default class Tabs extends React.Component {
   constructor(props) {
     super(props);
@@ -11,9 +37,19 @@ export default class Tabs extends React.Component {
   };
 
   render() {
+    let selectedTab = this.props.tabs[this.state.selected];
     return(
       <div>
         <h1>Tabs</h1>
+        <div className="tabs">
+         <Headers
+          selectedTab = {this.state.selected}
+          tabs = {this.props.tabs}>  
+          </Headers>
+        </div>
+        <div className='tab-content'>
+          <p>{selectedTab.content}</p>
+        </div>
       </div>
     )
   }
