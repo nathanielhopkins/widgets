bd5a03ed26c675e56780adcb1ea55333bd7b84d3
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-29 15:36:42 -0700

    create weather.jsx in frontend folder; create framework for React Component Weather that exports; start Weather.render with div className weather and an h1 with content Weather

---
+++ frontend/weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Weather extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return(
+      <div className="weather">
+        <h1>Weather</h1>
+      </div>
+    );
+  }
+}
