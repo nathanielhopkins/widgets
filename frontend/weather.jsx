5361019797a0d25bbd737c944133b629adfc5ed4
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-30 11:55:40 -0700

    write method getWeather in Weather that makes an API request to openweather at givin location and for now just console logs the result; bind getWeather to this in constructor; store api key in constructor; write method componentDidMount that uses navigator.geolocation.getCurrentPosition and uses getWeather as a callback with that location

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -3,6 +3,37 @@ import React from 'react';
 export default class Weather extends React.Component {
   constructor(props) {
     super(props)
+    
+    this.apiKey = '44c786ec5bdf4f23b852fd62061e29aa'
+    this.getWeather = this.getWeather.bind(this);
+
+  }
+ 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => this.getWeather(location));
+  }
+
+  getWeather(location) {
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = function () {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        if (req.status == 200) {
+          console.log(req.responseText);
+        }
+        else if (req.status == 400) {
+          alert('There was an error 400');
+        }
+        else {
+          alert('something else other than 200 was returned');
+        }
+      }
+    };
+    let lat = location.coords.latitude;
+    let long = location.coords.longitude;
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=` + lat + '&lon=' + long + `&appid=` + this.apiKey + '&units=imperial';
+    req.open("GET", url, true);
+    req.send();
 
   }
 
