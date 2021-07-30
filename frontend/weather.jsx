2aa14c14ba3160c1ebff014f7fc263c1d3fd2730
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-07-30 13:25:20 -0700

    initialize state.weather as "Loading current weather..." in constructor; render this.state.weather in Weather.render; create method updateWeather that is called by getWeather when the XMLHttpRequest.onreadystatechange callback returns successfully; bind updateWeather to this in constructor

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -6,7 +6,11 @@ export default class Weather extends React.Component {
     
     this.apiKey = '44c786ec5bdf4f23b852fd62061e29aa'
     this.getWeather = this.getWeather.bind(this);
+    this.updateWeather = this.updateWeather.bind(this);
 
+    this.state = {
+      weather: "Loading current weather..."
+    };
   }
  
   componentDidMount() {
@@ -19,7 +23,8 @@ export default class Weather extends React.Component {
     req.onreadystatechange = function () {
       if (req.readyState == XMLHttpRequest.DONE) {
         if (req.status == 200) {
-          console.log(req.responseText);
+          let res = req.responseText;
+          this.updateWeather(res)
         }
         else if (req.status == 400) {
           alert('There was an error 400');
@@ -28,19 +33,27 @@ export default class Weather extends React.Component {
           alert('something else other than 200 was returned');
         }
       }
-    };
+    }.bind(this);
     let lat = location.coords.latitude;
     let long = location.coords.longitude;
     let url = `http://api.openweathermap.org/data/2.5/weather?lat=` + lat + '&lon=' + long + `&appid=` + this.apiKey + '&units=imperial';
     req.open("GET", url, true);
     req.send();
+  }
 
+  updateWeather(res) {
+    console.log(res);
   }
 
   render() {
     return(
-      <div className="weather">
+      <div>
         <h1>Weather</h1>
+        <div className="weather">
+          <article>
+            <p>{this.state.weather}</p>
+          </article>
+        </div>
       </div>
     );
   }
