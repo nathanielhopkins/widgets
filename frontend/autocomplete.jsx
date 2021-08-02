ce91ac9192da0f4855f4b0d3ad30ab97a0717783
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-02 11:53:10 -0700

    write shell for React Component Autocomplete

---
+++ frontend/autocomplete.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class AutoComplete extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Autocomplete</h1>
+        <div className="autocomplete">
+          
+        </div>
+      </div>
+    )
+  }
+}
